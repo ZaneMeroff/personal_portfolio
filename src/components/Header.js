732078a18422aa81415a13cdc9bc42b2f0de5668
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-08 14:05:25 -0600

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+
   render() {
+
     let resumeData = this.props.resumeData;
+
     return (
       <React.Fragment>
 
@@ -23,8 +26,7 @@ export default class Header extends Component {
          <div className="row banner">
             <div className="banner-text">
                <h1 className="responsive-headline">{resumeData.name}</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
-               </h3>
+               <h3 style={{color:'#fff', width:'80%', fontFamily:'sans-serif '}}>{resumeData.roleDescription}</h3>
                <hr/>
                <ul className="social">
                   {
