0fd61febb8fd0779825938e32f15d8a84984e78e
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-08 18:34:01 -0600

    Update data

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+
+let resumeURL = 'https://alumni.turing.io/sites/default/files/resumes/Zachary%20Nemeroff%20Resume2020.pdf';
+
 export default class ContactUs extends Component {
+
   render() {
     let resumeData = this.props.resumeData;
     return (
@@ -7,17 +11,18 @@ export default class ContactUs extends Component {
           <div className="row section-head">
             <div className="ten columns">
               <p className="lead">
-              Feel free to contact me for any work or suggestions below
+              Feel free to contact me for work, suggestions, or networking!
               </p>
             </div>
           </div>
           <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
+                <h4>Zachary Nemeroff</h4>
+                <h4>Denver, CO</h4>
+                <h4>ZaNemeroff@gmail.com</h4>
               </div>
+              <a href={resumeURL} target='_blank'><button className='pdf-resume-button'>view resume.pdf</button></a>
             </aside>
           </div>
         </section>
