7375713d62c3bd547d7d2478227d3ea92ed31ac2
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-11 16:30:33 -0600

    Add simon link to menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,6 +18,7 @@ export default class Header extends Component {
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
+               <li><a className="smoothscroll" href="#simon">Simon</a></li>
                <li><a className="smoothscroll" href="#resume">Experience</a></li>
                <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
