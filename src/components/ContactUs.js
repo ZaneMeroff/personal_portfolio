a952fd03f3ec8d7ecb29b51f6b05ae6b319e0bd9
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-11 17:51:59 -0600

    Add alumni page link

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 let resumeURL = 'https://alumni.turing.io/sites/default/files/resumes/Zachary%20Nemeroff%20Resume2020.pdf';
+let turingAlumniLink = 'https://alumni.turing.io/alumni/zachary-nemeroff';
 
 export default class ContactUs extends Component {
 
@@ -21,6 +22,7 @@ export default class ContactUs extends Component {
                 <h4>Zachary Nemeroff</h4>
                 <h4>Denver, CO</h4>
                 <h4>ZaNemeroff@gmail.com</h4>
+                <a href={turingAlumniLink} target='_blank'><h4 style={{color: '#11ABB0', textDecorationLine: 'underline'}}>Turing School Alumni Page</h4></a>
               </div>
               <a href={resumeURL} target='_blank'><button className='pdf-resume-button'>view resume.pdf</button></a>
             </aside>
