9b123daba881b6ba8810e8e11b06f867795549c8
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-12 10:54:58 -0600

    Update email link

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-let resumeURL = 'https://alumni.turing.io/sites/default/files/resumes/Zachary%20Nemeroff%20Resume2020.pdf';
+let resumeURL = 'https://alumni.turing.io/sites/default/files/resumes/Zachary_Nemeroff_resume_2020.pdf';
 let turingAlumniLink = 'https://alumni.turing.io/alumni/zachary-nemeroff';
 
 export default class ContactUs extends Component {
@@ -21,8 +21,10 @@ export default class ContactUs extends Component {
               <div className="widget">
                 <h4>Zachary Nemeroff</h4>
                 <h4>Denver, CO</h4>
-                <h4>ZaNemeroff@gmail.com</h4>
-                <a href={turingAlumniLink} target='_blank'><h4 style={{color: '#11ABB0', textDecorationLine: 'underline'}}>Turing School Alumni Page</h4></a>
+                <a href='mailto: ZaNemeroff@gmail.com' target='_blank'><h4 tabindex='0' style={{color: '#11ABB0', textDecorationLine: 'underline'}}>ZaNemeroff@gmail.com</h4></a>
+                <div className='turing-alumni-link'>
+                  <a href={turingAlumniLink} target='_blank'><h4 tabindex='0' style={{color: '#11ABB0', textDecorationLine: 'underline'}}>Turing School Alumni Page</h4></a>
+                </div>
               </div>
               <a href={resumeURL} target='_blank'><button className='pdf-resume-button'>view resume.pdf</button></a>
             </aside>
