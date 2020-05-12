7aaf93be961aebab6da69c5c3f5dad9d6941f8b8
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-12 10:55:25 -0600

    Make header tabable

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,12 +16,12 @@ export default class Header extends Component {
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-               <li><a className="smoothscroll" href="#simon">Simon</a></li>
-               <li><a className="smoothscroll" href="#resume">Experience</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               <li tabindex='0' className="current nav-menu"><a className="smoothscroll" href="#home">Home</a></li>
+               <li tabindex='0' className='nav-menu'><a className="smoothscroll" href="#about">About</a></li>
+               <li tabindex='0' className='nav-menu'><a className="smoothscroll" href="#simon">Simon</a></li>
+               <li tabindex='0' className='nav-menu'><a className="smoothscroll" href="#resume">Experience</a></li>
+               <li tabindex='0' className='nav-menu'><a className="smoothscroll" href="#portfolio">Projects</a></li>
+               <li tabindex='0' className='nav-menu'><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
 
@@ -34,7 +34,7 @@ export default class Header extends Component {
                   {
                     resumeData.socialLinks && resumeData.socialLinks.map(item =>{
                       return(
-                              <li key={item.name}>
+                              <li tabindex='0' className='social-icons' key={item.name}>
                                 <a href={item.url} target="_blank"><i className={item.className}></i></a>
                               </li>
                             )
@@ -45,8 +45,8 @@ export default class Header extends Component {
             </div>
          </div>
 
-         <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+         <p className="scrolldown" tabindex='0'>
+            <a className="smoothscroll down-circle" href="#about"><i className="icon-down-circle"></i></a>
          </p>
 
       </header>
